66e49af76a1b4b2269c3ada47f7fddcc5ac12808
Cristopher-Leo <Criz5794@gmail.com>
2024-09-15 03:27:33 -0600

    desarrollo

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-import pandas as pd  
+import pandas as pd 
+import matplotlib.pyplot as plt 
 import streamlit as st   
 import plotly.express as px
-2222
+
+data = pd.read_csv('./vehicles_us.csv')
+data.fillna(0, inplace = True)
+hist_button = st.button('Construir histograma') 
+        
+if hist_button:
+    
+
+    st.write('Creación de un histograma')
+            
+    fig = px.histogram(data , x="odometer")
+
+    st.plotly_chart(fig)
+  
